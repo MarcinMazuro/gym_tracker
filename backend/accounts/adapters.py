936235c941dfbd8690ffdf63042cafb10494d459
Marcin <mazuromarcin@gmail.com>
2025-10-23 18:15:40 +0200

    fix email verification url

--- backend/accounts/adapters.py
+++ backend/accounts/adapters.py
@@ -3,4 +3,4 @@ from django.urls import reverse
 
 class CustomAccountAdapter(DefaultAccountAdapter):
     def get_email_confirmation_url(self, request, emailconfirmation):
-        return reverse('accounts:account_confirm_email', args=[emailconfirmation.key])
+        return request.build_absolute_uri(reverse('accounts:account_confirm_email', args=[emailconfirmation.key]))
