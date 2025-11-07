d36c6f6e6fcb0a8e82f37d5f33705d4a0a2cf81e
Marcin <mazuromarcin@gmail.com>
2025-11-07 23:13:08 +0100

    deploy to gcp

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_ue-1y##0dfy(*)7mvyp0j=o8tpdemlaa^n_mup+(j%dv43r+='
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True  # Set to True for development to see detailed error pages
